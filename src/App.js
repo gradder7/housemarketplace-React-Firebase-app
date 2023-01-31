4cbcb3ee3a22d750df300856bbba87bd0727f77c
Himanshu <mehra3228@gmail.com>
2023-01-31 13:24:46 +0530

    routes inserted

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import Explore from "./pages/Explore";
+import ForgetPassword from "./pages/ForgetPassword";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 function App() {
   return (
-    <div>
-    <h1>My App</h1>
-    </div>
+<>
+  <Router>
+    <Routes>
+    <Route path="/" element={<Explore/>}/>
+    <Route path="/offers" element={<Offers/>}/>
+    <Route path="/profile" element={<Profile/>}/>
+    <Route path="/sign-in" element={<SignIn/>}/>
+    <Route path="/sign-in" element={<SignUp/>}/>
+    <Route path="/forgot-password" element={<ForgetPassword/>}/>
+    </Routes>
+    {/* navbar component */}
+  </Router>
+</>
   );
 }
 
